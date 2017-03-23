2424208da99223c403cfaeed6088f78e5d4f48ae
葛尧 <geyao2012@gmail.com>
2017-03-23 17:47:06 +0800

    可以在tomcat成功部署了，还没有进行具体的测试。

---
+++ src/main/java/tool/log4j/MyDailyRollingFileAppender.java
@@ -0,0 +1,14 @@
+package tool.log4j;
+
+import org.apache.log4j.*;
+
+/**
+ * 删除本文件，会导致lo4j.properties工作异常
+ * Created by GR on 2017/2/20.
+ */
+public class MyDailyRollingFileAppender extends DailyRollingFileAppender{
+    public boolean isAsSevereAsThreshold(Priority priority) {
+        //只判断相同的范围（层级）
+        return this.getThreshold().equals(priority);
+    }
+}
